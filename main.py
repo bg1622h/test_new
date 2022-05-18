5712eb1941ab371f509639e227f3a8d9b5045bf7
bg1622h <71500540+bg1622h@users.noreply.github.com>
2022-05-18 17:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -366,9 +366,9 @@ if __name__ == '__main__':
     sample_std=xs.std()
     del xs
     del tr
-    random.seed(1338)
-    np.random.seed(1338)
-    xs, mark = sample_data(100, 100, xsf, xst)
+    random.seed(1339)
+    np.random.seed(1339)
+    xs, mark = sample_data(500, 500, xsf, xst)
     # print(xs)
     # mark = np.array(mark)
     # xs = np.array(xs)
@@ -402,7 +402,7 @@ if __name__ == '__main__':
     # xs=np.array(temp) # first - угу, second - ага
     # del temp
     #weak_classifier1 = build_weak_classifier(2, xs, mark, features)  # пока срать на то, что было
-    weak_classifier2 = build_weak_classifier(10, xs, mark, features)
+    weak_classifier2 = build_weak_classifier(25, xs, mark, features)
     for x in weak_classifier2:
         print(json.dumps(x, default=obj_json))
     #pool = multiprocessing.Pool()
